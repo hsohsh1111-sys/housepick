e43c0d538ef403f99eba8a3475c02f8c3837212f
hsohsh1111-sys <hsohsh1111@gmail.com>
2026-08-19 15:34:36 +0900

    first commit

---
+++ src/main/java/com/onrender/housepick/HousepickApplication.java
@@ -0,0 +1,13 @@
+package com.onrender.housepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousepickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onrender/housepick/HousepickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrender.housepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousepickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
